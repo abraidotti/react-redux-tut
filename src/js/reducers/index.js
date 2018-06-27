39e5ae2a36a1e8fbb1411da65c8f7c491737b8e9
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-06-27 07:41:48 -0400

    refactor root reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -2,6 +2,13 @@ const initialState = {
   articles: []
 };
 
-const rootReducer = (state = initialState, action) => state;
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return {...state, ...articles: [...state.articles, action.payload] };
+    default:
+      return state;
+  }
+};
 
 export default rootReducer;
